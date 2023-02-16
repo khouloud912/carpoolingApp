551365734a3171a65bed61bc9bcd83f45de3adf5
khouloud <k.sedghiani@autobiz.com>
2023-02-16 17:19:02 +0100

    add carpooler

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/login";
 import Layout from "./components/layout";
+import OfferForm from "./pages/carpooler/offerForm";
+import Details from "./pages/carpooler/details";
 
 
 function App() {
@@ -12,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Auth />} />
         <Route path="/nav" element={<Layout />} />
+        <Route path="/offerForm" element={<OfferForm />} />
+        <Route path="/detailOffer" element={<Details />} />
+
+
 
       </Routes>
   );
