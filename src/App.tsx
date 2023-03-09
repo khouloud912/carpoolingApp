b9c15f714448ab37058b43142ddd0be0768bb160
khouloud <k.sedghiani@autobiz.com>
2023-03-09 11:49:59 +0100

    added dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/login";
-import Layout from "./components/layout";
 import OfferForm from "./pages/carpooler/offerForm";
 import Details from "./pages/carpooler/details";
+import Dashboard from "./pages/Client/dashboard";
+
 
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Auth />} />
-        <Route path="/nav" element={<Layout />} />
         <Route path="/offerForm" element={<OfferForm />} />
         <Route path="/detailOffer" element={<Details />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
 
 
